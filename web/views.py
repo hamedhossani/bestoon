e3a0be48e9ab8eab3c9cb899b8401f3096bf8646
hamed <hamed.hossani@gmail.com>
2018-04-30 09:44:15 -0700

    add exponse and income simple rest api + add token model

--- web/views.py
+++ web/views.py
@@ -1,3 +1,49 @@
+from  json import JSONEncoder
 from django.shortcuts import render
+from  django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+from web.models import User,Token,Expense,Income
+from datetime import datetime
 # Create your views here.
+@csrf_exempt
+def submit_expense(request):
+    """ user submit an expense """
+#    return HttpResponse ('we are hear')
+    #print("i am in submit")
+    #print(request.POST)
+
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    date_now =  datetime.now()
+    this_text = request.POST['text']
+    Expense.objects.create(
+            user = this_user,
+            amount=request.POST['amount'],
+            date = date_now,
+            text = this_text
+            )
+
+    return JsonResponse(
+                        {
+                            'status':'ok',
+                        },encoder=JSONEncoder)
+@csrf_exempt
+def submit_income(request):
+    """ user submit an expense """
+
+    this_token = request.POST['token']
+    this_user = User.objects.filter( token__token = this_token ).get()
+    date_now =   datetime.now()
+    this_text = request.POST['text']
+    Income.objects.create(
+            user = this_user,
+            amount=request.POST['amount'],
+            date = date_now,
+            text = this_text
+            )
+
+    return JsonResponse(
+                        {
+                            'status':'ok',
+                        },encoder=JSONEncoder)
